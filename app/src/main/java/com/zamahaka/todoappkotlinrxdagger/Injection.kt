88274048d377f8641c4677ccf9c7068de1fed5ea
Yura <yustetsic@gmail.com>
2017-06-26 11:19:43 +0300

    Add dbHelper

--- app/src/main/java/com/zamahaka/todoappkotlinrxdagger/Injection.kt
+++ app/src/main/java/com/zamahaka/todoappkotlinrxdagger/Injection.kt
@@ -1,18 +1,15 @@
 package com.zamahaka.todoappkotlinrxdagger
 
 import android.content.Context
-import com.zamahaka.todoappkotlinrxdagger.data.source.remote.TasksRemoteDataSource
 import com.zamahaka.todoappkotlinrxdagger.data.source.TasksRepository
-import com.zamahaka.todoappkotlinrxdagger.util.schedulers.SchedulerProvider
+import com.zamahaka.todoappkotlinrxdagger.data.source.remote.TasksRemoteDataSource
 
 /**
  * Created by Yura Stetsyc on 6/23/17.
  */
 object Injection {
-    fun provideTasksRepository(context: Context) {
-        return TasksRepository.getInstance(
-                TasksRemoteDataSource.getInstance(),
-                TasksLocalDataSource.getInstance(context, SchedulerProvider)
-        )
+    fun provideTasksRepository(context: Context): TasksRepository {
+        return TasksRepository.getInstance(TasksRemoteDataSource.getInstance(),
+                TasksRemoteDataSource.getInstance())
     }
 }
